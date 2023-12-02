3262898b0cb7bdb05f38379140d3f6e4b71f490b
NeoCortex97 <35258671+NeoCortex97@users.noreply.github.com>
2023-12-02 15:45:48 +0100

    made the years and days be sorted in main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pathlib
 import subprocess
 
-for directory in filter(lambda item: item.is_dir(), pathlib.Path(".").iterdir()):
+for directory in sorted(filter(lambda item: item.is_dir(), pathlib.Path(".").iterdir())):
     if directory.name[0].isdigit():
         print(f'{directory.name}:')
-        for day in filter(lambda item: item.is_dir(), directory.iterdir()):
+        for day in sorted(filter(lambda item: item.is_dir(), directory.iterdir())):
             print(f'  {day.name}:')
             for script in filter(lambda item: item.is_file() and item.suffix.startswith(".py"), day.iterdir()):
                 proc = subprocess.Popen(f'python3 {script}'.split(" "), stdout= subprocess.PIPE)
