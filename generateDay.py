defe2df069624491b243af0d0299b1b99291da9a
NeoCortex97 <35258671+NeoCortex97@users.noreply.github.com>
2023-12-02 14:42:48 +0100

    added a script to generate a new day.

---
+++ generateDay.py
@@ -0,0 +1,37 @@
+import os
+import pathlib
+
+from aocd import get_data
+
+lines = [
+    "import pathlib",
+    "",
+    'with pathlib.Path(__file__).parent.joinpath("input.txt").open("r") as file:',
+    "    pass",
+    "",
+]
+
+
+if __name__ == "__main__":
+    year: int = int(input("Please select year: "))
+    day: int = int(input("Please select day: "))
+
+    if day < 1 or day > 25:
+        print("Day must be between 1 and 25!")
+        exit(1)
+
+    year_path = pathlib.Path(f'{year}')
+    if not year_path.exists():
+        year_path.mkdir()
+
+    day_path = year_path.joinpath(f'day{day}')
+    if not day_path.exists():
+        day_path.mkdir()
+        os.system(f"aocd {day} {year} > {day_path.joinpath('input.txt')}")
+
+    for i in range(1, 3):
+        part_path = day_path.joinpath(f"part{i}.py")
+        if not part_path.exists():
+            with part_path.open("w") as file:
+                for line in lines:
+                    file.write(line + "\n")
