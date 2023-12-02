c400d26303e03049223925430501c11d53775eca
NeoCortex97 <35258671+NeoCortex97@users.noreply.github.com>
2023-12-02 14:44:22 +0100

    solved 2022/1

---
+++ 2022/day1/part2.py
@@ -0,0 +1,13 @@
+import pathlib
+from typing import List
+
+with pathlib.Path(__file__).parent.joinpath("input.txt").open("r") as file:
+    elves: List[int] = []
+    elve: int = 0
+    for line in file.readlines():
+        if line.rstrip() != "":
+            elve += int(line)
+        else:
+            elves.append(elve)
+            elve = 0
+    print(sum(sorted(elves, reverse=True)[:3]))
