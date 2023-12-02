c1bf01e011558c784578afba710d70d883c6deb4
NeoCortex97 <35258671+NeoCortex97@users.noreply.github.com>
2023-12-02 12:57:40 +0100

    solved day2

--- 2023/day1/part1.py
+++ 2023/day1/part1.py
@@ -1,4 +1,4 @@
 import pathlib
 
-with pathlib.Path("2023/day1/day01.txt").open("r") as file:
+with pathlib.Path(__file__).parent.joinpath("input.txt").open("r") as file:
     print(sum([int(j[0] + j[-1]) for j in [[i for i in line.strip() if i.isdigit()] for line in file.readlines()]]))
